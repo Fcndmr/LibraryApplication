5f947347e8cde55d66ad3b7414115180afdb7d80
Efe Can Demir <efecandemir7887@gmail.com>
2025-03-18 22:34:53 +0300

    NavBar updated

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,6 +1,15 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function NavBar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("user"));
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("user"); 
+    setIsLoggedIn(false);
+    navigate("/user");
+  };
   return (
     <>
       <div className="bg-gray-700 p-3 flex justify-center">
@@ -9,7 +18,16 @@ function NavBar() {
         <NavLink to="/author" className="text-white p-2 text-2xl hover:bg-gray-600 hover:text-gray-200 hover:rounded-lg" href="#">Yazarlar</NavLink>
         <NavLink to="/contact" className="text-white p-2 text-2xl hover:bg-gray-600 hover:text-gray-200 hover:rounded-lg" href="#">İletişim</NavLink>
         <NavLink to="/about" className="text-white p-2 text-2xl hover:bg-gray-600 hover:text-gray-200 hover:rounded-lg" href="#">Hakkımızda</NavLink>
-        <NavLink to="/user" className="text-white p-2 text-2xl hover:bg-gray-600 hover:text-gray-200 hover:rounded-lg" href="#">Giriş Yap</NavLink>     
+        {isLoggedIn ? (
+        <button onClick={handleLogout} className="text-white p-2 text-2xl hover:bg-red-600 hover:text-gray-200 hover:rounded-lg">
+          Çıkış Yap
+        </button>
+      ) : (
+        <NavLink to="/user" className="text-white p-2 text-2xl hover:bg-gray-600 hover:text-gray-200 hover:rounded-lg">
+          Giriş Yap
+        </NavLink>
+      )}
+      
       </div>
     </>
   )
