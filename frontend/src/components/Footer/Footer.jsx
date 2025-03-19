24624a724c75aaf8c40b0976ca529a0080bd302b
Efe Can Demir <efecandemir7887@gmail.com>
2025-03-19 21:44:50 +0300

    About page added, footer updated

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@
 function Footer() {
   return (
     <div className="bg-gray-800 text-center text-lg p-3 text-white">
-      Footer
+      <p className="text-sm">&copy; {new Date().getFullYear()} Tüm Hakları Saklıdır.</p>
     </div>
   );
 }
--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -3,7 +3,28 @@
 function AboutPage() {
   return (
     <>
-      <div>Hakkımızda</div>
+        <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-6">
+            <div className="max-w-3xl bg-white p-8 rounded-lg shadow-lg text-center">
+                <h1 className="text-3xl font-bold text-gray-800 mb-4">Hakkımızda</h1>
+                <p className="text-gray-600 text-lg">
+                    Bu proje, kullanıcıların kütüphanelerini sanal ortama taşımasını olanak sağlamaktadır. Bu sayede sahip olduğu kitapları yazarları kolayca bulabilir.
+                </p>
+
+                <h2 className="text-2xl font-semibold text-gray-700 mt-6">Özellikler</h2>
+                <ul className="text-gray-600 text-lg list-disc text-left mt-2 mx-auto w-3/4">
+                    <li>Kitap, yazar, yayınevi, kategori ve alıntı ekleme.</li>
+                    <li>Kitap listesi ve detay sayfası</li>
+                    <li>Yazar bilgileri ve eserleri</li>
+                    <li>Arama ve filtreleme özellikleri</li>
+                    <li>Kullanıcı profili ve çıkış yapma</li>
+                </ul>
+
+                <h2 className="text-2xl font-semibold text-gray-700 mt-6">Amacımız</h2>
+                <p className="text-gray-600 text-lg">
+                    Kütüphanenizi online platforma taşımak.
+                </p>
+            </div>
+        </div>
     </>
   )
 }
